a6ab44d09c8adf0e03844cf0deb0a258d38a6e18
201611072 박승훈 <psh970718@naver.com>
2022-06-09 15:15:26 +0900

    웹 상세페이지 생성

--- web_main/web_main/urls.py
+++ web_main/web_main/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('recipe', include('web.urls')),
+    path('recipe/', include('web.urls')),
     path('admin/', admin.site.urls),
 ]
