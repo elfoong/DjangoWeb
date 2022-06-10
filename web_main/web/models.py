ff3c017ce4969a1f9e7958c08f9754cf4210ac5c
201611072 박승훈 <psh970718@naver.com>
2022-06-10 22:47:14 +0900

    정적 파일 및 미디어 파일 처리, 글에 이미지 및 파일 첨부 추가

--- web_main/web/models.py
+++ web_main/web/models.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.db import models
 
 
@@ -7,13 +9,23 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='web/images/%Y/%m/%d/', blank=True)
+    attatched_file = models.FileField(upload_to='web/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # author :
 
-    def __str__(self):
-        return f'[{self.pk}]{self.title}'
 
-    def get_absolute_url(self):
-        return f'/recipe/{self.pk}/'
+# 모델 메소드 정의(오버라이드)
+def __str__(self):
+    return f'[{self.pk}]{self.title}'
+
+
+def get_absolute_url(self):
+    return f'/recipe/{self.pk}/'
+
+
+def get_file_name(self):
+    return os.path.basename(self.attached_file.name)
--- web_main/web_main/settings.py
+++ web_main/web_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +120,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
