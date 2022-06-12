629841f9ec59b6e5d2cf2b3bc40d95edbfb6da08
201611072 박승훈 <psh970718@naver.com>
2022-06-12 22:38:59 +0900

    카테고리 모델 생성, 글 카테고리 필드 추가

--- web_main/web/models.py
+++ web_main/web/models.py
@@ -5,17 +5,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural: 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='web/images/%Y/%m/%d/', blank=True)
-    attatched_file = models.FileField(upload_to='web/files/%Y/%m/%d/', blank=True)
+    attached_file = models.FileField(upload_to='web/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
 
 # 모델 메소드 정의(오버라이드)
