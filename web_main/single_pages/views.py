ec24fb1119820ab4b6f0c85051d3d67ecb8161bd
201611072 박승훈 <psh970718@naver.com>
2022-06-14 05:17:03 +0900

    대문페이지, 자기소개 페이지 수정

--- web_main/single_pages/views.py
+++ web_main/single_pages/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from web.models import Post
 
 
 # Create your views here.
 def landing(request):
-    return render(
-        request,
-        'single_pages/landing.html'
-    )
+    return render(request, 'single_pages/landing.html',{
+        'recent_post' : Post.objects.order_by('-pk')[:3]
+    })
+
 
 
 def about_me(request):
