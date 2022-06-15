9b9d51126bc258cf56fd793674da99c22501fd01
201611072 박승훈 <psh970718@naver.com>
2022-06-15 18:32:08 +0900

    클라우드 배포 및 서비스 바인드

--- web_main/init_su.sh
+++ web_main/init_su.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "------ create default admin user ------"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@myapp.local', 'adminadmin')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('psh0718', 'admin@myapp.local', 'psh0718')" | python manage.py shell
